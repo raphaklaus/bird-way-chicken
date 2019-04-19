e83ff7e82ac695660b8a05d6cd322ca964d1fa81
Raphael Klaus <raphaklaus@gmail.com>
2019-04-19 16:56:25 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log('hmmm, chicken!')
+console.log('The password is...')
+console.log(process.env.PASSWORD)
+console.log('Heheh, dot env file versioned and encrypted, ' + 
+'you won\'t be able to know it hence you won\'t eat delicious chickens!')
